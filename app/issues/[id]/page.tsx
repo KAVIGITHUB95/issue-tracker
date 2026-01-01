2b4fb872d9573231afca569fbd5228226bb3fbe5
Kavishka Jayawardana <166035278+KAVIGITHUB95@users.noreply.github.com>
2026-01-01 13:29:08 +0530

    Revert "Add the edit button"

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,12 +1,9 @@
 import IssueStatusBadge from '@/app/components/IssueStatusBadge';
 import { prisma } from '@/prisma/client';
-import { Box, Button, Card, Flex, Grid, Heading, Text } from '@radix-ui/themes';
+import { Card, Flex, Heading, Text } from '@radix-ui/themes';
 import { notFound } from 'next/navigation';
 import React from 'react';
 import ReactMarkdown from "react-markdown";
-
-import { Pencil2Icon } from "@radix-ui/react-icons";
-import Link from 'next/link';
 
 interface Props {
     
@@ -26,9 +23,7 @@ const IssueDetailPage = async ({ params }: Props) => {
         notFound();
     return (
 
-        <Grid columns={{ initial: "1", md: "2"}} gap="5">
-
-            <Box>
+        <div>
 
             <Heading as="h2">{issue.title}</Heading>
             <Flex className="space-x-3" my="2">
@@ -41,22 +36,9 @@ const IssueDetailPage = async ({ params }: Props) => {
                 
                 <ReactMarkdown>{issue.description}</ReactMarkdown>
             </Card>
-            </Box>
-            <Box>
-
-                <Button>
-                    <Pencil2Icon />
-                    
-                    <Link href={`/issues/${issue.id}/edit`}>Edit Issue</Link>
-                    </Button>
-            
-            </Box>
-        
-        </Grid>
+        </div>
     )
-
-
-
 }
+
 
 export default IssueDetailPage

7553358b0cf70c0c201fb7af99aed50a14ac4a02
KAVIGITHUB95 <kavishkajayawardana34@gmail.com>
2026-01-06 09:58:21 +0530

    Fix caching issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -2,14 +2,15 @@ import { issueSchema } from "@/app/validationSchemas";
 import { prisma } from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function PATCH(request: NextRequest, { params }: { params: { id: string }}) {
+export async function PATCH(request: NextRequest, { params }: { params: Promise<{ id: string }> }) {
+    const { id } = await params;
     const body = await request.json();
     const validation = issueSchema.safeParse(body);
 
     if (!validation.success)
 
         return NextResponse.json(validation.error.format(), { status: 400 });
-    const issue = await prisma.issue.findUnique({ where: { id: parseInt(params.id) }});
+    const issue = await prisma.issue.findUnique({ where: { id: parseInt(id) }});
 
     if (!issue) return NextResponse.json({ error: "Invalid issue" }, { status: 400 });
     
